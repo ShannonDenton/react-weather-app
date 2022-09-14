2f600c65205507d24531aab400e8704d9045f9fc
ShannonDenton <denton.shannon7@gmail.com>
2022-09-14 17:06:46 -0500

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        {" "}
+        This project is coded by{" "}
+        <a href="https://shannon-denton.com/" target="_blank">
+          Shannon Denton
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/ShannonDenton/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on Github.
+        </a>
+      </footer>
+    </div>
+  );
 }
-
