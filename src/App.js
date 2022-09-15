45375bd608b13aaf61635539f9f93f90fea54c95
ShannonDenton <denton.shannon7@gmail.com>
2022-09-15 11:22:24 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        {" "}
-        This project is coded by{" "}
-        <a href="https://shannon-denton.com/" target="_blank">
-          Shannon Denton
-        </a>
-        and is{" "}
-        <a
-          href="https://github.com/ShannonDenton/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on Github.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          {" "}
+          This project is coded by{" "}
+          <a href="https://shannon-denton.com/" target="_blank">
+            Shannon Denton
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/ShannonDenton/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on Github.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
