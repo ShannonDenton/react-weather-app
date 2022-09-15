8c4d7b588fc1bfc16fb9d8b2028afc658f2177ff
ShannonDenton <denton.shannon7@gmail.com>
2022-09-15 16:39:45 -0500

    Added links and css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           {" "}
           This project is coded by{" "}
-          <a href="https://shannon-denton.com/" target="_blank">
+          <a
+            href="https://shannon-denton.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
             Shannon Denton
           </a>{" "}
           and is{" "}
           <a
             href="https://github.com/ShannonDenton/react-weather-app"
             target="_blank"
+            rel="noreferrer"
           >
             open-sourced on Github.
           </a>
---
+++ src/Weather.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Weather";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 7:00pm</li>
+        <li>Mostly cloudy</li>
+      </ul>
+      Hello from weather
+    </div>
+  );
+}
