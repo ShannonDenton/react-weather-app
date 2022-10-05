1037f7ac460c3afdd61ae4a36019a1bfe586e78f
ShannonDenton <denton.shannon7@gmail.com>
2022-10-05 17:58:15 -0500

    Fixed temp and icon misalignment and rounded wind

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -15,14 +15,16 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row">
         <div className="col-6">
-          <WeatherIcon code={props.data.icon} />
-          <WeatherTemperature fahrenheit={props.data.temperature} />
+          <div className="d-flex">
+            <WeatherIcon code={props.data.icon} />
+            <WeatherTemperature celsius={props.data.temperature} />
+          </div>
         </div>
 
         <div className="col-6">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {props.data.wind} km/h</li>
+            <li>Wind: {Math.round(props.data.wind)} km/h</li>
           </ul>
         </div>
       </div>
